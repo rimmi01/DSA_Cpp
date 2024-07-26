44a253c706a4a18f8fe74bb8d8d8fe348d3a8d32
Rimmi Goyal <goyalrimmi12@gmail.com>
2024-07-26 11:49:05 +0530

    sqrt with precision

---
+++ Searching/bookAllocation.cpp
@@ -0,0 +1,12 @@
+// Each student gets at least one book
+// each book should be allocated to a student 
+// book allocation should be in a contiguous manner
+// You have to allocate books to m students such that the maximum number of pages assigned to a student is minimum 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+}
